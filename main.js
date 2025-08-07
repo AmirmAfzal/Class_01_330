9d9a8f91e2e9fa3da2e66dc81835d8350f4fbcdc
AmirM Afzal <104265017+AmirmAfzal@users.noreply.github.com>
2025-08-07 06:24:43 -0700

    session 11

---
+++ main.js
@@ -0,0 +1,47 @@
+// console.log("Hello")
+// const var1 = 2
+// const var2 = "this is test text"
+// const var2 = 'this is test text'
+// const var2 = `this is test text`
+// const text = "It's"
+// const text = ""
+// console.log(var2)
+
+const number1 = 5
+const number2 = 10
+// console.log("this is the output : ",number1 , number2)
+
+// const sum = number1 + number2
+// const sum = number1 - number2
+// const sum = number1 * number2
+// const sum = number1 / number2
+// console.log(sum)
+
+
+// const var1 = 0
+// var1 = 10
+
+// let var2 = 0
+// console.log(var2)
+// var2 = 5
+// console.log(var2)
+
+
+// alert("Hi")
+
+
+const text = document.getElementById("title")
+text.innerHTML = "Amir"
+
+const desc = document.getElementById("description")
+desc.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae."
+
+
+function handleClick (){
+    alert("hi")
+    console.log("hello")
+}
+
+
+
+
