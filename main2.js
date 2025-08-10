0a0b1e6b110e5a43e69a308846252a3d32f67b05
AmirM Afzal <104265017+AmirmAfzal@users.noreply.github.com>
2025-08-10 06:26:59 -0700

    session12

---
+++ main2.js
@@ -0,0 +1,5 @@
+const text = document.getElementById("title")
+text.innerHTML = "Amirreza"
+
+const div = document.getElementById("container")
+div.innerHTML = "<p>This is a paragraph inside the container.</p>"
---
+++ main3.js
@@ -0,0 +1,54 @@
+// console.log("This is the main3.js file.");
+
+
+
+// const div = document.getElementById("container");
+// div.innerHTML = `<p class="paragraph">Lorem ipsum dolor sit amet.</p>`
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function testFunction(){
+//     const title = document.getElementById("title");
+//     title.innerText = "Hello"
+// }
+
+
+
+
+
+
+
+
+
+
+// function testFunction() {
+//     const title = document.getElementById("title");
+//     title.innerText = "Amir";
+//     title.style.backgroundColor = "#7162d2ff";
+//     title.style.display = "flex";
+// }
+// function testFunction() {
+//     const box = document.getElementById("box");
+//     box.style.display = "none";
+// }
+function remove(){
+    const box = document.getElementById("box")
+    box.style.display = "none";
+}
+
+
+
